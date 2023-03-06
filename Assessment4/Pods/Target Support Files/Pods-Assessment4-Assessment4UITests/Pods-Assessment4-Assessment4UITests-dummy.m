a91ff2557533cb51a0dddc64ee452a78431c19ac
rajkumar-kiwi <raj.kumar@kiwitech.com>
2023-03-06 17:48:52 +0530

    Revert "add"

--- Assessment4/Pods/Target Support Files/Pods-Assessment4-Assessment4UITests/Pods-Assessment4-Assessment4UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Assessment4_Assessment4UITests : NSObject
-@end
-@implementation PodsDummy_Pods_Assessment4_Assessment4UITests
-@end
--- Assessment4/Pods/Target Support Files/Pods-Assessment4Tests/Pods-Assessment4Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Assessment4Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Assessment4Tests
-@end
